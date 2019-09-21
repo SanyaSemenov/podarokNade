735ff829742a3a2f8ac7050500c461d252019c04
Саша Семенов <hooligan@MacBook-Pro-Sasa.local>
2019-09-21 15:25:35 +0300

    github pages test

--- src/modules/testing/testing.module.ts
+++ src/modules/testing/testing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { BaseQuestionComponent } from './components/question/base-question.component';
+import { TestingContainerComponent } from './components/testing-container/testing-container.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [BaseQuestionComponent, TestingContainerComponent],
 	imports: [CommonModule, MatButtonModule]
 })
 export class TestingModule {}
